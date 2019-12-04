50fc41e23907f45ea32d01c55fb73bf55e175091
yektan <yektan@outlook.com>
2019-12-05 00:21:50 +0300

    bootstrap 4 added, layout component structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SidebarComponent} from './layout/sidebar/sidebar.component';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
